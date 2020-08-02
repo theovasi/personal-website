9331acecef39ca7cc5f513d19229562975189df1
theovasi <theo.vasileiadis.work@gmail.com>
2020-08-03 00:04:15 +0300

    first commit.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './intro.css';
+import './sidebar.css';
+import './landing_page.css';
+
+
+class Sidebar extends React.Component {
+	render() {
+		return (
+			<div className="sidebar">
+				<div className="avatar">
+					<img src={require("./images/me.jpg")} alt="It's a me, Theo"/>
+				</div>
+			</div>
+		)
+	}
+}
+
+class Intro extends React.Component {
+	render() {
+		return (
+			<p>
+				Hello! My name is Theo. I am a software engineer.
+			</p>
+		)
+	}
+}
+
+class LandingPage extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Sidebar />
+				<Intro />
+			</div>
+		);
+	}
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<LandingPage />,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
